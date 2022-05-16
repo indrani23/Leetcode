e612475df40e1234da26ad8ff5ae1190d2713416
indrani23 <102457738+indrani23@users.noreply.github.com>
2022-05-16 15:20:33 +0530

    Time: 10 ms (10.98%), Space: 5.9 MB (32.11%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.cpp
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.cpp
@@ -4,9 +4,9 @@ public:
     {
         int cnt=0; // no of ways
         int k=1; // no of elements
-        for(int i=k;2*n>i*(i-1);i++)
+        for(int i=k;2*n>i*(i-1);i++) // to check for positive number
         {
-            if((n-((i-1)*i)/2) % i == 0)
+            if((n-((i-1)*i)/2) % i == 0) // to check if x(first element) is integer
                 cnt++;
         }
         return cnt;
