5163650a01ee9f4f4a617cea4f21edb5f5bbf226
indrani23 <102457738+indrani23@users.noreply.github.com>
2022-06-15 21:32:34 +0530

    Time: 4 ms (27.07%), Space: 6.3 MB (28.19%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     
     int removePalindromeSub(string s) {
-        return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
+        // return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
+        
+        if(s == "")
+            return 0;
+        for(int i=0,j=s.size()-1;i<j;i++,j--)
+        {
+            if(s[i]!=s[j])
+                return 2;
+        }
+        return 1;
+        
     }
 };
