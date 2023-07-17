90000654458b86b967e669d4ce5939375f42bf61
indrani23 <102457738+indrani23@users.noreply.github.com>
2023-07-17 22:19:13 +0530

    Added solution - LeetHub

---
+++ Look and Say Pattern - GFG/look-and-say-pattern.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    string lookandsay(int n) {
+        // code here
+        if(n==1) return "1";
+        if(n==2) return "11";
+        string s = "11";
+        for(int i=3;i<=n;i++)
+        {
+            string temp = "";
+            int cnt = 1;
+            s += "$";
+            for(int j=1;j<s.size();j++)
+            {
+                if(s[j-1] != s[j])
+                {
+                    temp += to_string(cnt);
+                    temp += s[j-1];
+                    cnt = 1;
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
+            s = temp;
+        }
+        return s;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution obj;
+        cout<<obj.lookandsay(n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
